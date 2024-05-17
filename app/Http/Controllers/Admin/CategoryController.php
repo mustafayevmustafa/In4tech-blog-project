0112123665f87fb530de938a08f867c37de7e833
mustafa99 <mustafayevmustafa99@gmail.com>
2024-05-17 21:57:34 +0400

    first commint

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = Category::get();
+        return view('admin.pages.categories.index', compact('categories'));
+    }
+
+    public function create(){
+        return view('admin.pages.categories.create');
+    }
+
+    public function store(CategoryStoreRequest $request){
+        $data = $request->validated();
+        Category::create($data);
+        return redirect()->route('admin.category.index');
+    }
+
+    public function delete(Request $request){
+         $id = $request->id;
+//        Category::where('id',$id)->delete();
+         Category::find($id)->delete();
+        return redirect()->route('admin.category.index');
+    }
+    public function edit($id){
+        $category = Category::find($id);
+        return view('admin.pages.categories.edit',compact('category'));
+    }
+    public function update(CategoryUpdateRequest $request){
+        $data = $request->validated();
+        $category = new Category();
+        $category->title = $data['title'];
+        $category->save();
+        return redirect()->route('admin.category.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\MustafaController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('admin/index',[IndexController::class,'index'])->name('admin.index');
+Route::get('admin/category',[CategoryController::class,'index'])->name('admin.category.index');
+Route::get('admin/category/create',[CategoryController::class,'create'])->name('admin.category.create');
+Route::post('admin/category/store',[CategoryController::class,'store'])->name('admin.category.store');
+Route::post('admin/category/delete',[CategoryController::class,'delete'])->name('admin.categories.delete');
+Route::get('admin/category/edit/{id}',[CategoryController::class,'edit'])->name('admin.categories.edit');
+Route::post('admin/category/update',[CategoryController::class,'update'])->name('admin.categories.update');
